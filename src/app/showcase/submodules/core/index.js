d7456aace6ac1e45d2b583cc6571e7d35c914677
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2016-06-06 14:29:05 -0500

    trying to set up a firebasedata service... a fun adventure in modularization, but the example project I'm using has different ways of instantiating firbase, so I'm cutting losses and simplifying

--- src/app/showcase/submodules/core/index.js
+++ src/app/showcase/submodules/core/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
+
 module.exports = require('angular')
     .module('bookbottles.showcase.core', [
-    	// require() // NEEDED! REQUIRE FIREBASE_URL_HERE
+    	require('angularfire')
     ])
     .factory('FirebaseDataService', require('./services/FirebaseDataService'))
     .name;
--- src/app/showcase/submodules/core/services/FirebaseDataService.js
+++ src/app/showcase/submodules/core/services/FirebaseDataService.js
@@ -4,7 +4,11 @@ module.exports = FirebaseDataService;
 
 // based on: https://github.com/gordonmzhu/angular-course-demo-app-v2/blob/master/src/app/core/firebaseData.service.js
 
+FirebaseDataService.$inject = ['Firebase'];
+
 function FirebaseDataService(/*FIREBASE_URL maybe later via a Constants service,*/ Firebase) {
+
+	console.log(Firebase);
 
 	var root = new Firebase(/*FIREBASE_URL*/ 'https://fighter-jets.firebaseio.com'); // just the url; no auth just yet.
 
